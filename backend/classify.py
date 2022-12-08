2badc7ba3ecc30fc1866905144f72297c5dbc3be
HordesOfGhost <85671929+HordesOfGhost@users.noreply.github.com>
2022-12-08 14:38:23 +0545

    Add Nepali

--- backend/classify.py
+++ backend/classify.py
@@ -2,24 +2,28 @@ import numpy as np
 import joblib
 from backend.calculatetfidf import ProcessText,calc_tf_idf
 import ast
+from Nepali_nlp import LanguageTranslation
 model=joblib.load('model.pkl')
 Category_class=['business', 'entertainment', 'politics', 'sport', 'tech']
 
-
+#idf values
 with open('backend/idf.txt') as f:
      idf_load=f.read()
 idf=ast.literal_eval(idf_load)
 
-print(calc_tf_idf("abandon abandon abandon so abc abc   ability",idf)[0:5])
 
 def count_word(word):
     cnt=word.count(" ")+1
     return cnt
 
-def Predict_news(txt):
+def Predict_news(news):
+    if not news[0:10].isascii():
+        conv = LanguageTranslation()
+        txt= conv.to_english(news[0:(len(news)/2)])
+        txt=' '.join(txt)
     text=ProcessText(txt)
     tf_idf=calc_tf_idf(txt,idf) 
-    if all(i <.05 for i in tf_idf) or count_word(txt)<100:
+    if all(i <.05 for i in tf_idf) or count_word(news)<100:
         return("You cannot classify this text.")
     else:
         tf_idf_2=calc_tf_idf(" ",idf)
